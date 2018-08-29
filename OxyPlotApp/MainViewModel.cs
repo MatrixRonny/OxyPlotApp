96a90c57eb45b159bde00f1e7cbb9dba7b56574a
MatrixRonny <35771215+MatrixRonny@users.noreply.github.com>
2018-08-29 14:27:52 +0300

    Added project to Git

---
+++ OxyPlotApp/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlotApp
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            FirstViewModel firstVm = new FirstViewModel();
+            SecondViewModel secondVm = new SecondViewModel();
+
+            NavigationItems = new ArrayList
+            {
+                firstVm,
+                secondVm,
+                new IntermediateViewModel(secondVm)
+            };
+        }
+
+        public ArrayList NavigationItems { get; private set; }
+    }
+}
